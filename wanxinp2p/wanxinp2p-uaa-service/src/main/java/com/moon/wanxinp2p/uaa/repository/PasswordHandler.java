f5d9636eedb88dd7f67e42bdeae97b05316985eb
MooNkirA <lje888@126.com>
2022-02-22 13:50:12 +0800

    feat: 初始化项目 uaa 认证服务工程

---
+++ wanxinp2p/wanxinp2p-uaa-service/src/main/java/com/moon/wanxinp2p/uaa/repository/PasswordHandler.java
@@ -0,0 +1,15 @@
+package com.moon.wanxinp2p.uaa.repository;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+public abstract class PasswordHandler {
+
+    private PasswordHandler() {
+    }
+
+    public static String encode(String password) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
+    }
+}
